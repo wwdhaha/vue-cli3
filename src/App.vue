4cb487f76f1e35dec6df0d069ccda38f58dfdc55
wangweidong <17731172741@163.com>
2020-09-05 17:46:23 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {
+    // 设置title
+    // this.setTitle();
+    // 设置rem
+    this.setPageRem();
+    window.addEventListener("resize", this.setREM);
+  },
+  methods: {
+    methods: {
+      // 按照分辨率设置rem的size
+      setPageRem() {
+        const initRem = 100;
+        const initScreenWidth = 1920;
+        const screenWidth = window.screen.width; // document.body.clientWidth
+        const currRem = (initRem / initScreenWidth) * screenWidth;
+        document.body.parentElement.style.fontSize = currRem + "px";
+      }
+      // 设置主页面浏览器页头标题
+      // setTitle() {
+      //   document.title = "heiheihei";
+      // }
+    }
+  }
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import Vue from "vue";
+
+import ElementUI from "element-ui";
+import "./styles/element-variables.scss";
+Vue.use(ElementUI);
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
